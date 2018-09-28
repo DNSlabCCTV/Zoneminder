0713d0d1a086dcb03edd646e9476e4b3750ce38b
Sung Wung, Yeom <yeom4032yeom4032@gmail.com>
2018-09-28 11:05:01 +0900

    Update test.js

--- TestAPI/test.js
+++ TestAPI/test.js
@@ -1,20 +1,22 @@
-var Zoneminder = require(__dirname + "/index");
+var Zoneminder = require(__dirname + "/Module");
 
 var rtspUrl = [];
 rtspUrl.push('rtsp://192.168.1.106:8555/unicast');
-//rtspUrl.push('rtsp://admin:dns3438@192.168.1.107:88/videoMain');
-rtspUrl.push('rtsp://admin:dns5303438@192.168.1.102:88/videoMain');
+
 
 var host = '192.168.1.104';
 var ports = 8180;  //  web port  - ports[0]
 var cameraName = 'Test';
 var callback = '';
 
+// Set Up Camera
 Zoneminder.ZoneminderSetup(host, ports, cameraName, rtspUrl, callback,function(result,data){
 	if (result){
 	    console.log(result);
 	}
 });
+
+// Delete Camera
 /*
 Zoneminder.ZoneminderDeleteMonitor(host, ports, cameraName, callback,function(result,data){
 	if (result){
